bca7c734b5f46b47558184b785cf6fa8d3c9146d
牛亮 <nll@itcast.cn>
2019-08-06 09:00:34 +0800

    父给子传

---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Child</h1>
+    {{ number }}
+  </div>
+</template>
+
+<script>
+export default {
+  // 接收外界传的值
+  props: {
+    number: {
+      // 验证类型
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Parent</h1>
+
+    <child :number="count"></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+  components: {
+    Child
+  },
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
